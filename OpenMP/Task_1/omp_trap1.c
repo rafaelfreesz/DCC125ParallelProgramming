9a221718ab549bee725a34b7d00aeb105661efdd
Rafael F. R. Correa <rafaelfreesz@gmail.com>
2023-05-11 17:24:18 -0300

    Task_1 OpenMP

---
+++ OpenMP/Task_1/omp_trap1.c
@@ -0,0 +1,108 @@
+/* File:    omp_trap1.c
+ * Purpose: Estimate definite integral (or area under curve) using trapezoidal 
+ *          rule.
+ *
+ * Input:   a, b, n
+ * Output:  estimate of integral from a to b of f(x)
+ *          using n trapezoids.
+ *
+ * Compile: gcc -g -Wall -fopenmp -o omp_trap1 omp_trap1.c
+ * Usage:   ./omp_trap1 <number of threads>
+ *
+ * Notes:   
+ *   1.  The function f(x) is hardwired.
+ *   2.  In this version, each thread explicitly computes the integral
+ *       over its assigned subinterval, a critical directive is used
+ *       for the global sum.
+ *   3.  This version assumes that n is evenly divisible by the 
+ *       number of threads
+ *
+ * IPP:  Section 5.2.1 (pp. 216 and ff.)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+void Usage(char* prog_name);
+double f(double x);    /* Function we're integrating */
+void Trap(double a, double b, int n, double* global_result_p);
+int main(int argc, char* argv[]) {
+   double  global_result = 0.0;  /* Store result in global_result */
+   double  a=0.0;
+   double b=10.000;                 /* Left and right endpoints      */
+   int     n=atof(argv[2]);                    /* Total number of trapezoids    */
+   int     thread_count;
+   double begin_time, end_time, elapsed_time; 
+   
+   if (argc != 3) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   
+   if (n % thread_count != 0) Usage(argv[0]);
+   
+   begin_time=omp_get_wtime();
+   
+#  pragma omp parallel num_threads(thread_count) 
+   Trap(a, b, n, &global_result);
+   
+   end_time=omp_get_wtime();
+   elapsed_time=end_time-begin_time;
+   printf("Elapsed time: %lf\n", elapsed_time);
+   
+   FILE* file;
+   file=fopen("output.txt","a+");
+   fprintf(file,"%d - %d - %lf\n", thread_count, n,elapsed_time);
+   fclose(file);
+   
+   return 0;
+}  /* main */
+/*--------------------------------------------------------------------
+ * Function:    Usage
+ * Purpose:     Print command line for function and terminate
+ * In arg:      prog_name
+ */
+void Usage(char* prog_name) {
+   fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+   fprintf(stderr, "   number of trapezoids must be evenly divisible by\n");
+   fprintf(stderr, "   number of threads\n");
+   exit(0);
+}  /* Usage */
+/*------------------------------------------------------------------
+ * Function:    f
+ * Purpose:     Compute value of function to be integrated
+ * Input arg:   x
+ * Return val:  f(x)
+ */
+double f(double x) {
+   double return_val;
+   return_val = x*x;
+   return return_val;
+}  /* f */
+/*------------------------------------------------------------------
+ * Function:    Trap
+ * Purpose:     Use trapezoidal rule to estimate definite integral
+ * Input args:  
+ *    a: left endpoint
+ *    b: right endpoint
+ *    n: number of trapezoids
+ * Output arg:
+ *    integral:  estimate of integral from a to b of f(x)
+ */
+void Trap(double a, double b, int n, double* global_result_p) {
+   double  h, x, my_result;
+   double  local_a, local_b;
+   int  i, local_n;
+   int my_rank = omp_get_thread_num();
+   int thread_count = omp_get_num_threads();
+   h = (b-a)/n; 
+   local_n = n/thread_count;  
+   local_a = a + my_rank*local_n*h; 
+   local_b = local_a + local_n*h; 
+   my_result = (f(local_a) + f(local_b))/2.0; 
+   for (i = 1; i <= local_n-1; i++) {
+     x = local_a + i*h;
+     my_result += f(x);
+   }
+   my_result = my_result*h; 
+#  pragma omp critical 
+   *global_result_p += my_result; 
+}  /* Trap */
